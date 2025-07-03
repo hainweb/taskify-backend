1241df0937b26cf98cc570961e30736b264de708
hain <hainofficialweb@gmail.com>
2025-07-03 22:54:27 +0530

    feat(backend): setup MongoDB connection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+const { default: connectDB } = require('./config/db');
 
 var app = express();
 
@@ -17,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+connectDB()
 
 app.use('/', indexRouter);
 
