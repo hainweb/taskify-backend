e23e455b266227fb028304a7b4602defba3df633
hain <hainofficialweb@gmail.com>
2025-07-05 23:06:12 +0530

    feat(backend): add auth controller with register

---
+++ controllers/authController.js
@@ -0,0 +1,30 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+// @desc Register User
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
