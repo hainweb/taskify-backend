83c3b10baabf656bf3e961ff11de735b4bd8f3d3
hain <hainofficialweb@gmail.com>
2025-07-08 23:16:40 +0530

    Fix issues

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,14 @@
-import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
-import protect from "../middleware/authMiddleware.js";
+const express = require("express");
+const { registerUser, loginUser } = require("../controllers/authController.js");
+const protect = require("../middleware/authMiddleware.js");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-
 router.get("/profile", protect, (req, res) => {
   res.json(req.user);
 });
-
 
 export default router;
