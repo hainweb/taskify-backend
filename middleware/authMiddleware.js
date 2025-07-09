9f49ab6ff590eaedc1ac128eebc1c448e3aef361
hain <hainofficialweb@gmail.com>
2025-07-09 23:21:30 +0530

    Fix issues

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,13 @@
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.js");
 
 const protect = async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
