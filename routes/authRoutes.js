0eabb6f79258ae2ab0a6be378b9d64caad4a7234
hain <hainofficialweb@gmail.com>
2025-07-28 23:56:57 +0530

    feat(backend): add update profile route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { registerUser, loginUser, updateProfile } = require("../controllers/authController.js");
 const protect = require("../middleware/authMiddleware.js");
-
 const router = express.Router();
 
 router.post("/register", registerUser);
