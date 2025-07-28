7a12bbe18b6aaf0f10eab909b3ab43d88e3e2dd5
hain <hainofficialweb@gmail.com>
2025-07-28 23:54:26 +0530

    feat(backend): add update profile controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,3 +50,30 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// @desc Update user profile
+export const updateProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser.id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      token: generateToken(updatedUser.id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
